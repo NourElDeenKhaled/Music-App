72fcf78e67c6d5cbfaf67e3f76f6cdfeee906b0b
Nour El Deen Khaled <nourkhaled15@hotmail.com>
2021-05-20 01:25:07 +0200

    Added API public link

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -11,20 +11,20 @@ const initialValues = {
 const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialValues);
   const fetchArtistsData = async () => {
-    // const response = await fetch(
-    //   "https://my-json-server.typicode.com/NourElDeenKhaled/Music-App/artists"
-    // );
-    const response = await fetch("http://localhost:8888/artists");
+    const response = await fetch(
+      "https://my-json-server.typicode.com/NourElDeenKhaled/Music-App/artists"
+    );
+    // const response = await fetch("http://localhost:8888/artists");
     const data = await response.json();
     dispatch({ type: "SET_ARTISTS_DATA", payload: data });
   };
 
   const fetchAlbumsData = async () => {
-    // const response = await fetch(
-    //   "https://my-json-server.typicode.com/NourElDeenKhaled/Music-App/artists"
-    // );
+    const response = await fetch(
+      "https://my-json-server.typicode.com/NourElDeenKhaled/Music-App/albums"
+    );
 
-    const response = await fetch("http://localhost:8888/albums");
+    // const response = await fetch("http://localhost:8888/albums");
     const data = await response.json();
     dispatch({ type: "SET_ALBUMS_DATA", payload: data });
   };
